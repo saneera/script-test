7fbff6246435eeb76cc95d48f0b9064289febd0c
Smudiyan <vEzG-WYxwCSaReYFMJ7k>
2020-03-24 15:03:35 +1030

    Initial commit

--- kafka-deploy-scripts/04-deploy-kafka-bridge.sh
+++ kafka-deploy-scripts/04-deploy-kafka-bridge.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-
-#!/bin/bash
 set +x
 
 PROPERTY_FILE=config.properties
@@ -12,15 +10,20 @@ function getProperty {
 }
 
 NAMESPACE=$(getProperty "namespace")
-CLUSTERNAME=$(getProperty "cluster.name")
-BRIDGETNAME="${CLUSTERNAME}-bridge"
-BOOTSTRAP="${CLUSTERNAME}-kafka-bootstrap:9092"
+CLUSTER_NAME=$(getProperty "cluster.name")
+BRIDGE_NAME="${CLUSTER_NAME}"
+BOOTSTRAP_SERVER="${CLUSTER_NAME}-kafka-bootstrap:9092"
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 
 # ensure to be on the right namespace
 oc project $NAMESPACE 2> /dev/null || oc new-project $NAMESPACE
 
-kubectl apply -f $DIR/resources/kafka-bridge.yaml -n $NAMESPACE
+echo "Applying bridge resources"
+
+cat $DIR/resources/kafka-bridge.yaml \
+  | sed "s/name: .*/name: $BRIDGE_NAME/" \
+  | sed "s/bootstrapServers: .*/bootstrapServers: $BOOTSTRAP_SERVER/" \
+  | kubectl apply -f - -n $NAMESPACE
 
 
--- kafka-deploy-scripts/05-deploy-kafka-connect.sh
+++ kafka-deploy-scripts/05-deploy-kafka-connect.sh
@@ -13,12 +13,20 @@ function getProperty {
 
 NAMESPACE=$(getProperty "namespace")
 export CLUSTERNAME=$(getProperty "cluster.name")
+BOOTSTRAP_SERVER="${CLUSTER_NAME}-kafka-bootstrap:9093"
+SECRET_NAME="${CLUSTER_NAME}-cluster-ca-cert"
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 
 # ensure to be on the right namespace
 oc project $NAMESPACE 2> /dev/null || oc new-project $NAMESPACE
 
-kubectl apply -f $DIR/resources/kafka-connect.yaml -n $NAMESPACE
+echo "Applying bridge resources"
+
+cat $DIR/resources/kafka-connect.yaml \
+  | sed "s/name: .*/name: $CLUSTERNAME/" \
+  | sed "s/name: .*/secretName: $SECRET_NAME/" \
+  | sed "s/bootstrapServers: .*/bootstrapServers: $BOOTSTRAP_SERVER/" \
+  | kubectl apply -f - -n $NAMESPACE
 
 
