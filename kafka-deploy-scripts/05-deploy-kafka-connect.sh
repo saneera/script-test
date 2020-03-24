a32a530c59b58bc6812c014c5f4a32e714073ab7
Smudiyan <vEzG-WYxwCSaReYFMJ7k>
2020-03-24 14:05:47 +1030

    Initial commit

---
+++ kafka-deploy-scripts/05-deploy-kafka-connect.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+#!/bin/bash
+set +x
+
+PROPERTY_FILE=config.properties
+
+function getProperty {
+   PROP_KEY=$1
+   PROP_VALUE=`cat $PROPERTY_FILE | grep "$PROP_KEY" | cut -d'=' -f2`
+   echo $PROP_VALUE
+}
+
+NAMESPACE=$(getProperty "namespace")
+CLUSTERNAME=$(getProperty "cluster.name")
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+
+# ensure to be on the right namespace
+oc project $NAMESPACE 2> /dev/null || oc new-project $NAMESPACE
+
+kubectl apply -f $DIR/resources/kafka-connect.yaml -n $NAMESPACE
+
+
